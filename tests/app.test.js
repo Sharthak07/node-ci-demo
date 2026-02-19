bb9bdce776daf0d535058d0676f662e2c670695b
sarthak <sarthakdas07@gmail.com>
2026-02-19 13:20:04 +0530

    changes made

--- tests/app.test.js
+++ tests/app.test.js
@@ -6,7 +6,7 @@ const app = require('../src/app');
 test('GET / returns welcome message', async () => {
   const res = await request(app).get('/');
   expect(res.statusCode).toBe(200);
-  expect(res.body.message).toBe('Hello from CI pipeline!');
+  expect(res.body.message).toBe('Hello from CI pipeline');
 });
 
 // Test 2 - Add route
